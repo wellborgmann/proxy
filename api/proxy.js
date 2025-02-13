08fdcce60d48ef8b22c460e7d23ab89e46d00833
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2025-02-13 05:00:00 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,1 +1,18 @@
+// api/proxy.js
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+export default function handler(req, res) {
+  const url = req.query.url;
+  if(!url)return
+  const proxy = createProxyMiddleware({
+    target: url, // Substitua pelo domínio de destino
+    changeOrigin: true,                    // Altera a origem da requisição para o domínio de destino
+    secure: false,                         // Desabilita a validação de SSL (útil para desenvolvimento)
+    pathRewrite: {
+      '^/api/proxy': '',                   // Opcional: remova '/api/proxy' do caminho da URL
+    },
+  });
+
+  return proxy(req, res);  // Executa o proxy
+}
 
