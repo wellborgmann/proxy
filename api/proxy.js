1630db8b92b2b9612958927e6b62b0283c35e0e7
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2025-02-13 05:25:39 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,18 +1,42 @@
-const axios = require('axios');
+const { createProxyMiddleware } = require("http-proxy-middleware"); 
+const { createServer } = require("http");
+const https = require("https");
+const http = require("http");
+const path = require("path");
+const httpServer = createServer(app);
+const bodyParser = require("body-parser");
 
-export default async function handler(req, res) {
-  const fileUrl = req.query.url;
 
-  if (!fileUrl) {
-    return res.status(400).send('Erro: Parâmetro "url" ausente');
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(express.json({ limit: "50mb" })); // Certifique-se de que esta linha também esteja presente
+
+app.use("/download", (req, res, next) => {
+  const targetUrl = req.query.url;
+
+  if (!targetUrl) {
+    return res.status(400).send("A URL de destino não foi fornecida.");
   }
 
-  try {
-    const response = await axios.get(fileUrl, { responseType: 'arraybuffer' });
-    res.setHeader('Content-Type', 'application/octet-stream');
-    res.setHeader('Content-Disposition', 'attachment; filename="lista.m3u"');
-    res.send(response.data);
-  } catch (error) {
-    res.status(500).send('Erro ao baixar o arquivo');
-  }
-}
+  req.proxyUrl = targetUrl;
+  next();
+});
+
+app.use(
+  "/download",
+  createProxyMiddleware({
+    changeOrigin: true,
+    pathRewrite: (path, req) => {
+      // Reescreva o caminho removendo o "/download"
+      const newPath = req.proxyUrl.replace(/^\/download/, "");
+      return newPath;
+    },
+    router: (req) => {
+      // Define a URL de destino dinamicamente
+      return req.proxyUrl;
+    },
+  })
+);
+
+httpServer.listen(7000, () => {
+  console.log(`Servidor HTTPS rodando na porta `);
+});
