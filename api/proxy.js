0a258faf6e3e0821ecaf187535eaa248cf4f13fc
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2025-02-13 05:14:44 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,19 +1,23 @@
-// api/proxy.js
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-export default function handler(req, res) {
-  const url = req.query.url;
-  if(!url)return
+module.exports = function handler(req, res) {
+  // Pega o parâmetro da URL 'url' da query string
+  const targetUrl = req.query.url;
+
+  if (!targetUrl) {
+    return res.status(400).send('URL missing in query parameter');
+  }
+
   const proxy = createProxyMiddleware({
-    target: url, // Substitua pelo domínio de destino
-    changeOrigin: true,                    // Altera a origem da requisição para o domínio de destino
-    secure: false,                         // Desabilita a validação de SSL (útil para desenvolvimento)
-    pathRewrite: {
-      '^/api/proxy': '',                   // Opcional: remova '/api/proxy' do caminho da URL
+    target: targetUrl,  // Define a URL de destino como a URL fornecida na query
+    changeOrigin: true,
+    secure: false,  // Desabilitar validação SSL (útil em ambientes de desenvolvimento)
+    onProxyRes: (proxyRes, req, res) => {
+      // Copiar cabeçalhos de resposta do servidor de destino
+      res.setHeader('Content-Type', proxyRes.headers['content-type']);
+      res.setHeader('Content-Disposition', proxyRes.headers['content-disposition']);
     },
-     followRedirects: true,   
   });
 
-  return proxy(req, res);  // Executa o proxy
-}
-
+  return proxy(req, res);
+};
