024b709b7c89e15106a5ec509ec4e5c7ddc79b6a
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2025-02-13 05:16:54 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function handler(req, res) {
-  // Pega o parâmetro da URL 'url' da query string
   const targetUrl = req.query.url;
 
   if (!targetUrl) {
@@ -9,11 +8,12 @@ module.exports = function handler(req, res) {
   }
 
   const proxy = createProxyMiddleware({
-    target: targetUrl,  // Define a URL de destino como a URL fornecida na query
-    changeOrigin: true,
-    secure: false,  // Desabilitar validação SSL (útil em ambientes de desenvolvimento)
+    target: targetUrl,  // URL de destino
+    changeOrigin: true,  // Mudar a origem do cabeçalho para o destino
+    secure: false,       // Desabilitar a verificação SSL
+    followRedirects: true,  // Seguir redirecionamentos
     onProxyRes: (proxyRes, req, res) => {
-      // Copiar cabeçalhos de resposta do servidor de destino
+      // Manter os cabeçalhos de resposta do servidor de destino
       res.setHeader('Content-Type', proxyRes.headers['content-type']);
       res.setHeader('Content-Disposition', proxyRes.headers['content-disposition']);
     },
